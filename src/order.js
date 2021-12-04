6e20bd884afabc9f284f7abed1c5350e3bca8c6b
Casey Halstead <catherineclosee@gmail.com>
2021-12-04 17:08:26 -0600

    Initial commit orders

--- src/order.js
+++ src/order.js
@@ -1,7 +1,44 @@
 
+
+function takeOrder(orders, orderReciever){
+  if (orderReciever.length !== 3) {
+    return orderReciever.push(orders);
+  }
+}
+
+
+function refundOrder(number, orderReciever){
+  for (var i = 0; i < orderReciever.length; i++){
+  if (orderReciever[i].orderNumber === number) {
+    return orderReciever.splice(i, 1)
+  }
+  }
+}
+
+
+function listItems(orderList){
+  var itemsHolder = [];
+  for (var i = 0; i < orderList.length; i++){
+    itemsHolder.push(orderList[i].item);
+  }
+  return itemsHolder.join(", ")
+}
+
+
+function searchOrder(orderList, food){
+  var foodHolder = [];
+  for (var i = 0; i < orderList.length; i++){
+    foodHolder.push(orderList[i].item);
+  } if (foodHolder.includes(food)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
