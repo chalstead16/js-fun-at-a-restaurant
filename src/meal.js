682ba42c4d9aa48aed8e0da0f088843c6cf0a6ad
Casey Halstead <catherineclosee@gmail.com>
2021-12-03 20:45:09 -0600

    Initial commit - add warm-up and meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,54 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName){
+  return `Delicious ${menuItemName}`
 }
 
+
+var deliciousFood = nameMenuItem();
+
+function createMenuItem(deliciousFood, price, type){
+  var menuItem = {
+    name: deliciousFood,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+
+var ingredients = [];
+
+function addIngredients(foods, menuItemIngredients) {
+  if (menuItemIngredients.indexOf(foods) === -1){
+    menuItemIngredients.push(foods);
+  }
+    return menuItemIngredients;
+}
+
+
+function formatPrice(price){
+  return(`$${price}`);
+}
+
+
+function decreasePrice(price){
+  return price * 0.9;
+}
+
+
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
