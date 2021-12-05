ed398eedbfab75ea0e062b22c4f8dc0bb121115b
Casey Halstead <catherineclosee@gmail.com>
2021-12-04 20:47:37 -0600

    Initial commit restaurant test

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,32 @@
+//the test code hard codes the name of the restaurant
+//so the function doesn't need to declare a name variable
+function createRestaurant(restaurantName){
+  return {
+    name:restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner:[],
+    }
+  }
+}
+
+//line 61 in test file establishes name in a variable
+function addMenuItem(restaurantName, meal){
+  if (meal.type === "breakfast" && restaurantName.menus.breakfast.includes(meal) === false){
+    return restaurantName.menus.breakfast.push(meal)}
+
+    else if (meal.type === "lunch" && restaurantName.menus.lunch.includes(meal) === false){
+    return restaurantName.menus.lunch.push(meal)}
+
+    else if (meal.type === "dinner" && restaurantName.menus.dinner.includes(meal) === false){
+    return restaurantName.menus.dinner.push(meal)}
+  }
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
